e770b499a633a9b416e7a3b63e8cea6c6268d652
jessliu0401 <125-jliu3@sd43.bc.ca>
2021-07-23 22:23:51 -0700

    finished

--- ProjectNumber1/src/Main.java
+++ ProjectNumber1/src/Main.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    //Code your solution to problem number one here
     static int problemOne(String s) {
-        int answer = 0;
-        for (int a = 0; a < s.length(); a++) {
+        int answer = 0;//this records how many vowels there are in the string
+        for (int a = 0; a < s.length(); a++) {//we scan the string letter by letter
+            //if the letter are equal to the vowels, we add 1 to the answer
             if (s.charAt(a) == 'a' || s.charAt(a) == 'e' || s.charAt(a) == 'i' || s.charAt(a) == 'o' || s.charAt(a) == 'u') {
+                answer++;
+            }
+        }
+        return answer;//we return the answer
+    }
+
+
+
+    static int problemTwo(String s) {
+        int answer = 0;//this records the final answer
+        for (int i = 0; i < s.length() - 2; i++) {
+            //we scan the string 3 letters once, and we move forward 1 index per scan
+            // for example, first we scan pos 012, then we scan pos 123
+            if (s.substring(i, i + 3).equals("bob")) {
+                // if the three letters are equal to bob, then we add 1 to answer
                 answer++;
             }
         }
@@ -11,47 +30,58 @@ public class Main {
     }
 
 
-    //Code you problem number two here
-    static int problemTwo(String s){
-        int answer = 0;
-        for (int i = 0; i < s.length() - 2; i++){
-            if ( s.substring(i, i+3).equals("bob")){
-                answer ++;
+
+
+
+
+    static String problemThree(String s) {
+        int startIndex = 0;//where the final string would start
+        int theLength =1;//the final length(when the whole string gets scanned, value of sLength would be implemented in here
+        int sLength = 1;//substring length(would reset during the program)
+
+        for (int i = 0;i < s.length()-1;i++){//scan the string
+            if (s.charAt(i) <= s.charAt(i+1)){//if they are in order
+
+                sLength =sLength+1;//the final string's length would be 1 longer
+                int a=i+1;
+                /*if during the scanning process, a longer alphabetical string is found
+                this part would replace the final indexes with the new alphabetical substring
+                 */
+                if (theLength<sLength){
+                    theLength = sLength;
+                    startIndex = a-(theLength-1);
+
+
+                }
+
+            }else {// if we get to this part, this means that none of the letters are in alphabetical order
+                //therefore the longest substring would be just one letter long
+                sLength =1;
             }
-        }
-        return answer;
 
 
-    //Code your solution to problem number 3 here
-        //Problem 3
-        // Assume s is a string of lower case characters.
-        //Write a program that prints the longest substring of s in which the letters occur in alphabetical order. For example, if s = 'azcbobobegghakl', then your program should print
-        //Longest substring in alphabetical order is: beggh
-        //In the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should print
-        //Longest substring in alphabetical order is: abc
-    static String problemThree(String s){
-        int pos = 0;
-        String[] strings;
-        for (int a = 0;  a < s.length() - 1;  a++){
+        }//we print the results here
+        String result = s.substring(startIndex,startIndex+theLength);
+        return result;
+    }
 
-            int result = s.substring(a,a).compareTo(s.substring(a+1,a+1));
 
-            if (result <= 0) {
-               strings [pos] += s.charAt(a);}
 
-            else pos ++;}
-            return strings;
 
-            public static void main(String[] args) {
-        /*
-        Set s to a string and run your method using s as the parameter
-        Run your method in a println statement to determine what the output was
-        Once you think you have it working try running the tests.
-        The tests will put your method through several different Strings to test
-        all possible cases.  If you have 100% success then there is no bugs in your methods.
-         */
-        String s = "bobby";
-        String[] final = problemThree(s);
-        System.out.println();
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        String answer = problemThree("yqpnlilestyiwmmqsqpv");
+        System.out.println(answer);
+
+
+
     }
 }
