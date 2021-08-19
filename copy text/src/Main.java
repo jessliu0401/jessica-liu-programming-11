95fcc60f55e70326cc4afdb0533b154c0dfe023d
jessliu0401 <125-jliu3@sd43.bc.ca>
2021-08-19 11:57:08 -0700

    upload

--- copy text/src/Main.java
+++ copy text/src/Main.java
@@ -6,10 +6,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //testing 
-        for (int num = 0; num < searchWord("IBM").length; num++) {
-            System.out.println("Your word is in:" + (searchWord("IBM")[num]));
-        }
+
         //write each line into arraylist
         ArrayList<String> textLine = new ArrayList<>();
         FileReader read = new FileReader("readtext.txt");
@@ -23,6 +20,11 @@ public class Main {
         for (int i = 0; i < textLine.size(); i++) {
             System.out.println(textLine.get(i));
         }
+
+        //testing
+        for (int num = 0; num < searchWord("IBM").length; num++) {
+            System.out.println("Your word is in:" + (searchWord("IBM")[num]));
+        }
     }
 
 //searchword method
@@ -30,12 +32,14 @@ public class Main {
         Path path = Paths.get("readtext.txt");
         String content = Files.readString(path);
         String[] paragraph = content.split(" ");
+
         int[] indexes = new int[10];
         int num = 0;
         for (int i = 0; i < paragraph.length; i++) {
             if (paragraph[i].equals(words)) {
                     indexes[num] = i;
                     num++;
+
                 }
             }
         return indexes;
