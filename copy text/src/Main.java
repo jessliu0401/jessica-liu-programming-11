4ae3921081de6996537179f87546f3ee511e05f4
jessliu0401 <125-jliu3@sd43.bc.ca>
2021-08-19 17:25:06 -0700

    fixed bugs

--- copy text/src/Main.java
+++ copy text/src/Main.java
@@ -22,28 +22,31 @@ public class Main {
         }
 
         //testing
-        for (int num = 0; num < searchWord("IBM").length; num++) {
-            System.out.println("Your word is in:" + (searchWord("IBM")[num]));
+        for (int num = 0; num < searchWord("when").length; num++) {
+            System.out.println("Your word is in:" + (searchWord("when")[num]));
         }
     }
 
-//searchword method
+    //searchword method
     public static int[] searchWord(String words) throws IOException {
         Path path = Paths.get("readtext.txt");
         String content = Files.readString(path);
         String[] paragraph = content.split(" ");
-
-        int[] indexes = new int[10];
-        int num = 0;
-        for (int i = 0; i < paragraph.length; i++) {
-            if (paragraph[i].equals(words)) {
-                    indexes[num] = i;
-                    num++;
-
-                }
+//count how many are here to determine size of the array needed
+        int size = 0;
+        for (int a = 0; a < paragraph.length; a++) {
+            if (paragraph[a].equals(words)) {
+                size++;
             }
+        }
+            int[] indexes = new int[size];
+            int num = 0;
+            for (int i = 0; i < paragraph.length; i++) {
+                if (paragraph[i].equals(words)) {
+                    indexes[num] = i;
+                    num++; } }
         return indexes;
         }
 
-    }
 
+}
